4ccf5c3dc7e30567b6cf385e803b1daeb7ece52d
ashfarfaizi <112808092+ashfarfaizi@users.noreply.github.com>
2026-07-31 21:48:09 +0530

    Time: 0 ms (100%), Space: 28.5 MB (25.39%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+
+        for (char c : word) {
+            freq[c - 'a']++;
+        }
+
+        sort(freq.rbegin(), freq.rend());
+
+        int pushes = 0;
+
+        for (int i = 0; i < 26; i++) {
+            int cost = (i / 8) + 1;
+            pushes += freq[i] * cost;
+        }
+
+        return pushes;
+    }
+};
