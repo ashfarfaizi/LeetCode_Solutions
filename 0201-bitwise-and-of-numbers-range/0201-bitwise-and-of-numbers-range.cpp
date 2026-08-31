e4b3cf3106f0f914e4fa9ba7b056bcf83749fdae
ashfarfaizi <112808092+ashfarfaizi@users.noreply.github.com>
2026-08-31 21:50:37 +0530

    Time: 0 ms (100%), Space: 11.1 MB (80.02%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int rangeBitwiseAnd(int left, int right) {
+        int shift = 0;
+        // Shift both numbers to the right until they are equal
+        while (left < right) {
+            left >>= 1;
+            right >>= 1;
+            shift++;
+        }
+        // Shift back to the left to restore the trailing zeros
+        return left << shift;
+    }
+};
