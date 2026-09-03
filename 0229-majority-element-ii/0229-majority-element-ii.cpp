a15273a9f0d2d969a6e4f92a1a34f4cead43ce5a
ashfarfaizi <112808092+ashfarfaizi@users.noreply.github.com>
2026-09-03 21:50:16 +0530

    Time: 0 ms (100%), Space: 23.8 MB (49.54%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> majorityElement(std::vector<int>& nums) {
+        int candidate1 = 0, candidate2 = 0;
+        int count1 = 0, count2 = 0;
+        
+        // Step 1: Find potential candidates using modified Boyer-Moore
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+        
+        // Step 2: Validate the candidates by counting their actual occurrences
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
+        
+        std::vector<int> result;
+        int threshold = nums.size() / 3;
+        if (count1 > threshold) result.push_back(candidate1);
+        if (count2 > threshold) result.push_back(candidate2);
+        
+        return result;
+    }
+};
