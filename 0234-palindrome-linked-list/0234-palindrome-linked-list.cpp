7967ef4d166140b942ff3fe8074d837cbd1f1885
ashfarfaizi <112808092+ashfarfaizi@users.noreply.github.com>
2026-09-05 19:16:12 +0530

    Time: 0 ms (100%), Space: 121.8 MB (42.59%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    // Helper function to reverse a linked list in-place
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return true;
+        }
+        
+        // Step 1: Find the middle of the linked list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        // Step 2: Reverse the second half of the list
+        ListNode* secondHalfHead = reverseList(slow->next);
+        
+        // Step 3: Compare both halves
+        ListNode* p1 = head;
+        ListNode* p2 = secondHalfHead;
+        bool isPalin = true;
+        
+        while (p2 != nullptr) {
+            if (p1->val != p2->val) {
+                isPalin = false;
+                break;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        
+        // Restore the original list configuration (Clean-up code)
+        slow->next = reverseList(secondHalfHead);
+        
+        return isPalin;
+    }
+};
